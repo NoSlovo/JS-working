68d73036fa392e83087bb3463c3b7ad489272ab6
Daniil <delimit1@yandex.ru>
2021-08-14 21:34:35 +0300

    JS zadaca

---
+++ JavaScript/script.js
@@ -0,0 +1,9 @@
+// if( x === 0 )cosnole.log('zero')else console.log('nozero');
+//что не так с кодом выше,и как исправить проблему?.
+
+if('0'== 0){
+    console.log("zero");
+}else{
+    console.log("nozero")
+}
+
